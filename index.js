e5b23a8c93615e20d06069335c75e76849b17029
mttmzr <mttmzr@users.noreply.github.com>
2021-12-13 13:56:41 +0100

    Update index.js

--- index.js
+++ index.js
@@ -4,14 +4,14 @@ const { program } = require('commander')
 
 program.version('0.1.1')
 
-program.option('-m', '--from-mnemonic <mnemonic>')
+program.option('-m', '--from-mnemonic <mnemonic>', 'mnemonic phrase to derive keys from')
 
 program.parse(process.argv)
 
 const main = async () => {
   const WABridge = await require('@mymonero/mymonero-monero-client')({})
 
-  const mnemonic = program.opts().mnemonic
+  const mnemonic = program.opts().fromMnemonic
 
   if (mnemonic) {
     console.log(WABridge.seedAndKeysFromMnemonic(mnemonic, 'MAINNET'))
